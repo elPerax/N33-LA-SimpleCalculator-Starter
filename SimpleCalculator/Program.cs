4e2574a950473ad370492476e27046466e251f28
samre <reyescifuentessamuel@gmail.com>
2024-09-09 13:27:04 -0400

    Finished change 2, loop works but requires restart of input upon a single wrong number.

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -13,14 +13,40 @@ namespace SimpleCalculator
 
                 // Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
+                int result1 = 0, result2 = 0;
+                bool firstNumber, secondNumber;
 
-                double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
-                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                bool completion = true;
+                while (completion)
+                {
+                    Console.WriteLine("Please input the first number:");
+                    firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine(), out result1);
+
+                    if (!firstNumber)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                        continue;
+                    }
+
+                    Console.WriteLine("Please input the second number:");
+                    secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine(), out result2);
+
+                    if (!secondNumber)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                        continue;
+                    }
+
+                    completion = false;
+                    break;
+                }
+
+                Console.WriteLine("Enter add or + to add the numbers together.");
                 string operation = Console.ReadLine();
 
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                double resultOperation = calculatorEngine.Calculate(operation, result1, result2);
 
-                Console.WriteLine(result);
+                Console.WriteLine(resultOperation);
 
             } catch (Exception ex)
             {
